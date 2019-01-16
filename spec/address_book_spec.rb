4e1701446c541d23eefd01f8a882052d70e69a03
EC2 Default User <ec2-user@ip-172-31-44-64.us-east-2.compute.internal>
2019-01-16 18:58:32 +0000

    checkpoint 4 models completed

---
+++ spec/address_book_spec.rb
@@ -0,0 +1,59 @@
+require_relative 'entry'
+
+    class AddressBook
+       attr_reader :entries
+       
+       def initialize
+         @entries = []
+       end
+    end
+    
+        def add_entry(name, phone_number, email)
+             index = 0
+             entries.each do |entry|
+               if name < entry.name
+                 break
+               end
+               index+= 1
+             end
+   require_relative '../models/address_book'
+   
+ 
+ RSpec.describe AddressBook do
+   describe "attributes" do
+     it "responds to entries" do
+       book = AddressBook.new
+       expect(book).to respond_to(:entries)
+      
+     it "initializes entries as an array" do
+       book = AddressBook.new
+       expect(book.entries).to be_an(Array)
+     end
+ 
+     it "initializes entries as empty" do
+       book = AddressBook.new
+       expect(book.entries.size).to eq(0)
+     end
+    end
+  end
+  
+ describe "#add_entry" do
+     it "adds only one entry to the address book" do
+       book = AddressBook.new
+       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+ 
+       expect(book.entries.size).to eq(1)
+     end
+ 
+     it "adds the correct information to entries" do
+       book = AddressBook.new
+       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       new_entry = book.entries[0]
+ 
+       expect(new_entry.name).to eq('Ada Lovelace')
+       expect(new_entry.phone_number).to eq('010.012.1815')
+       expect(new_entry.email).to eq('augusta.king@lovelace.com')
+     end
+   end
+ end
+end
