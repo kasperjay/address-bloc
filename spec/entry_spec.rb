598f76ca87a8ad8afeb44bed63c45ceb9d1f28b3
EC2 Default User <ec2-user@ip-172-31-44-64.us-east-2.compute.internal>
2019-01-17 00:05:49 +0000

    correcting checkpoint 4

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,53 +1,39 @@
-class Entry
-    attr_accessor :name, :phone_number, :email
-    def initialize(name, phone_number, email)
-        @name = name
-        @phone_number = phone_number
-        @email = email
-    end
-    def to_s
-     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
-    end
-    require_relative '../models/entry'
-    RSpec.describe Entry do
-        describe "attributes" do
-            it "responds to name" do
-                entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-                    expect(entry).to respond_to(:name)
-            end
-            
-            it "reports its name" do
-                entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-                expect(entry.name).to eq('Ada Lovelace')
-            end
+require_relative '../models/entry'
 
-            it "responds to phone number" do
-                entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-                expect(entry).to respond_to(:phone_number)
-            end
-            
-            it "reports its phone_number" do
-                entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-                expect(entry.phone_number).to eq('010.012.1815')
-            end
- 
-            it "responds to email" do
-                entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-                expect(entry).to respond_to(:email)
-            end
-                
-            it "reports its email" do
-                entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-                expect(entry.email).to eq('augusta.king@lovelace.com')
-            end
-        end
-        
-        describe "#to_s" do
-            it "prints an entry as a string" do
-                entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-                expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
-                expect(entry.to_s).to eq(expected_string)
-            end
-        end
+RSpec.describe Entry do
+  describe "attributes" do
+    let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
+
+    it "responds to name" do
+      expect(entry).to respond_to(:name)
     end
+
+    it "reports its name" do
+      expect(entry.name).to eq('Ada Lovelace')
+    end
+
+    it "responds to phone number" do
+      expect(entry).to respond_to(:phone_number)
+    end
+
+    it "reports its phone_number" do
+      expect(entry.phone_number).to eq('010.012.1815')
+    end
+
+    it "responds to email" do
+      expect(entry).to respond_to(:email)
+    end
+
+    it "reports its email" do
+      expect(entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
+
+  describe "#to_s" do
+    it "prints an entry as a string" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
+      expect(entry.to_s).to eq(expected_string)
+    end
+  end
 end
